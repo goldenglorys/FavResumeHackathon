5bb7eb3dc63ce816a32270a6878fda6560121709
goldenglorys <olusolagloryolamide@gmail.com>
2020-06-07 23:17:17 +0100

    Node js backend and vue cli frontend codebase stabilize

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-md-8">
+
+                <div class="card">
+
+                    <div class="card-header">My Example</div>
+
+                    <div class="card-body">
+
+                        Welcome to ItSolutionStuff.com
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+  
+
+<script>
+
+    export default {
+
+        mounted() {
+
+            console.log('Component mounted.')
+
+        }
+
+    }
